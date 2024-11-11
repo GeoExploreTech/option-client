d4f78bf4dd74b922fd442423f6a9dd2e187e8469
Omoware Bolarinwa <bware185@gmail.com>
2024-11-11 08:32:06 +0100

    update

--- js/libs/stream_data.js
+++ js/libs/stream_data.js
@@ -7,7 +7,7 @@ const wsUrl = "wss://ws.geoviso.com/ws/candles/NZDUSD_otc/60/1";
 // Function to create an observable for WebSocket messages
 function createWebSocketObservable(assetSym, duration, count) {
   const url = `wss://ws.geoviso.com/ws/candles/${assetSym}/${duration}/${count}`;
-  console.log(url);
+  console.log("HERE I MINE:", url);
 
   return new Observable((observer) => {
     // Create WebSocket connection
