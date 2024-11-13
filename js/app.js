08506022e7d62fcdaa093121602eafaa72f36c59
Omoware Bolarinwa <bware185@gmail.com>
2024-11-13 11:34:12 +0100

    fvefve

--- js/app.js
+++ js/app.js
@@ -146,13 +146,13 @@ function initVueApp() {
 
             return new Promise((resolve) => {
               const net = new brain.recurrent.LSTMTimeStep({
-                hiddenLayers: [7],
+                hiddenLayers: [5, 5],
               });
 
               // Train the network
               net.train(trainingData, {
                 iterations: 10000,
-                learningRate: 0.05,
+                learningRate: 0.01,
                 errorThresh: 0.005,
                 log: (error) => console.log(error),
                 logPeriod: 500,
